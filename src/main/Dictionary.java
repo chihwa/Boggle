e21fbdff2b2028fd5881751ac74407b4bb53c800
Chi Hwa Kim <chihwa93@gmail.com>
2015-03-07 01:39:44 -0600

    upate to 90K words dictionary

--- src/main/Dictionary.java
+++ src/main/Dictionary.java
@@ -5,17 +5,15 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Dictionary {
-    public static final String DICTIONARY = ".\\src\\main\\resources\\en_US.dic";
+    public static final String DICTIONARY = ".\\src\\main\\resources\\words.dic";
     public Trie trie = new Trie();
 
     public void buildDic() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(DICTIONARY));
 
-        String s;
-        int wordNum = Integer.valueOf(br.readLine());
-        for (int i = 0; i < wordNum; i++) {
-            s = br.readLine();
-            String word = s.split("/")[0];
+        String word;
+        while ((word = br.readLine()) != null) {
+            word = word.trim();
             trie.insertString(word.toUpperCase());
         }
     }
